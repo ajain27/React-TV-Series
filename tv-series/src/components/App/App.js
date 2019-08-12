3eacec0d7758199fc18e1d08135ad3afb8cfa2f3
Ankit Jain <ankit.jain67@t-mobile.com>
2019-08-12 12:53:12 -0700

    Props, State push

---
+++ tv-series/src/components/App/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import Intro from '../Intro/intro'
+import './App.css'
+
+class App extends Component {
+  state = {
+    series: []
+  }
+  render () {
+    return (
+      <div className='App'>
+        <header className='App-header'>
+          <h1 className='App-title'>TV Series List</h1>
+        </header>
+        <Intro message='Here you can find all of your most loved series' />
+        The length of series array - {this.state.series.length}
+      </div>
+    )
+  }
+}
+
+export default App
